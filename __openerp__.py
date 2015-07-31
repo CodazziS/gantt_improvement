5a3f1d39fcf8397d9471b982d8ca1faecb94bd68
Stéphane Codazzi <codazzi.stephane@gmail.com>
2015-07-31 12:07:40 +0200

    Update to Version 1.0

--- __openerp__.py
+++ __openerp__.py
@@ -1,24 +1,35 @@
 {
-    'name': "Gantt Improvement",
+    'name': "TeMPO - Gantt Improvement",
     'author': 'Stéphane Codazzi @ TeMPO-Consulting',
     'category': 'Project',
+    'website': 'http://tempo-consulting.fr/',
     'sequence': 1,
     'description': """
-Gantt Improvement
-=================
+=========================
+TeMPO - Gantt Improvement
+=========================
+
+"TeMPO - Gantt Improvement" replace the default gantt and add features :
+ - Update to latest DHTMLX gantt version
+ - Add diffenrents scales (Day, Week, Month, Year)
+ - Better integration
+
+ License: MIT
+
     """,
-    'version': '0.3',
+    'version': '1.0',
     'depends': ['web', 'web_gantt'],
     'js': [
         'static/src/js/gantt.js',
         'static/dhtmlxGantt/sources/dhtmlxgantt.js',
-    ], 
+        'static/dhtmlxGantt/ext/dhtmlxgantt_marker.js',
+    ],
     'css': [
         'static/src/css/gantt.css',
         'static/dhtmlxGantt/dhtmlxgantt.css',
     ],
     'qweb': ['static/src/xml/gantt.xml'],
     'data': [
-        'views/web_gantt.xml', #Odoo V8.0, comment this for Odoo V7
+        'views/web_gantt.xml',  # Odoo V8.0, comment this for Odoo V7
     ],
 }
